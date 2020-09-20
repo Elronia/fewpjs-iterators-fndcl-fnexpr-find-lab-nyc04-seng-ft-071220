01baabc22921711b92d63f58b8eed3e6421b1738
Elronia <zar.catherine@gmail.com>
2020-09-20 14:35:47 -0400

    Done.

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
-const testVar = {}
-
-function testFunc() {
-  return "hi"
+const superbowlWin = (record) => {
+    const result = record.find(record => record.result === "W")
+    // return !!result ? result.year : undefined
+    if (result) {
+      return result.year
+    }
+    else {
+      return undefined
+    }
 }
+
